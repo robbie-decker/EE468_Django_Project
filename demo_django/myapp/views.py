6e9f3b8dfe632f8517472bb813936eb9ca41ac9c
Robbie Decker <robbd56@gmail.com>
2022-04-08 02:24:48 -0400

    Displaying some generic info

--- demo_django/myapp/views.py
+++ demo_django/myapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Instructor, Department, Student
 
 # Create your views here.
+def index(request):
+    print(Instructor)
+    instructor_names = Instructor.objects.values_list("name")
+    context = {
+        'instructor_names' : instructor_names,
+    }
+    print(f"Here is some stuff: {instructor_names}")
+    print(instructor_names)
+    return render(request, 'myapp/index.html', context)
