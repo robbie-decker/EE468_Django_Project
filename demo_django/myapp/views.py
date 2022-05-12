031852878a4d6bbcd3ac4e9c29f3045806fcec76
Robbie Decker <robbd56@gmail.com>
2022-05-12 13:46:52 -0400

    Semi working professor list and salary table

--- demo_django/myapp/views.py
+++ demo_django/myapp/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Instructor, Department, Student
+from django.db.models import Avg, Min, Max
 
 # Create your views here.
 def index(request):
-    print(Instructor)
-    instructor_names = Instructor.objects.values_list("name")
+    department_min_max_avg = Instructor.objects.values("dept").annotate(avg_value=Avg("salary"), min_value=Min("salary"), max_value=Max('salary'))
+    # department_avg_salary = Instructor.objects.aggregate(Avg('salary'))
+    instructor_names = Instructor.objects.values('name').order_by('name')
     context = {
         'instructor_names' : instructor_names,
+        'department_min_max_avg': department_min_max_avg,
     }
     print(f"Here is some stuff: {instructor_names}")
-    print(instructor_names)
+    print(f"Here is some more stuff: {department_min_max_avg}")
     return render(request, 'myapp/index.html', context)
